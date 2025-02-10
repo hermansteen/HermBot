4ffb16639a81ddf0630a65961e4383329704ef58
Herman Steen <herst222@student.liu.se>
2025-02-10 08:40:47 +0100

    ping command

---
+++ eslint.config.js
@@ -0,0 +1,5 @@
+const js = require("@eslint/js");
+
+module.exports = {
+  rules: js.configs.recommended.rules,
+};
---
+++ index.js
@@ -0,0 +1,44 @@
+const { Client, Events ,GatewayIntentBits, Collection } = require("discord.js");
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds,GatewayIntentBits.MessageContent]
+});
+
+client.commands = new Collection();
+
+const fs = require("fs");
+const path = require("path");
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, (c) => {
+    console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+
+    if (!command) return;
+
+    await command.execute(interaction);
+});
+
+client.login(process.env.TOKEN);
